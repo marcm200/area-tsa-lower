987c979e6a832cb519d9f891acfef488cafb0a3a
marcm200 <52798815+marcm200@users.noreply.github.com>
2020-05-05 15:00:06 +0200

    Add files via upload

---
+++ _calculate_lower_area_bound.bat
@@ -0,0 +1,7 @@
+areatsa img lenm=13 lenj=11 parallel=8
+areatsa img lenm=14 lenj=12 parallel=8
+areatsa img lenm=15 lenj=13 parallel=8
+areatsa lenm=16 lenj=14 parallel=8
+areatsa lenm=17 lenj=15 parallel=8
+areatsa lenm=18 lenj=16 parallel=3
+pause
